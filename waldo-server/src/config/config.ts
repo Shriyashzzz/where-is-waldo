4da73ccf88ea8cc4b14af65b4c26097aea6bd4d1
Shriyash <ghimires1@my.smccd.edu>
2026-09-03 23:07:44 -0700

    feat: scaffold db schema

--- waldo-server/src/config/config.ts
+++ waldo-server/src/config/config.ts
@@ -4,10 +4,12 @@ dontenv.config();
 
 interface ConfigProp {
   port: number;
+  DATABASE_URL: string;
 }
 
 const config: ConfigProp = {
   port: Number(process.env.PORT || 4000),
+  DATABASE_URL: process.env.DATABASE_URL!,
 };
 
 export default config;

eedb70081cd246558798f7379c1a8b6dcac61895
Shriyash <ghimires1@my.smccd.edu>
2026-09-05 13:47:42 -0700

    fix: magnifying glass stuck to zoomed portion

--- waldo-server/src/modals/seed.ts
+++ waldo-server/src/modals/seed.ts
@@ -2,12 +2,38 @@ import "dotenv/config";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/client";
-
 const connectionString = `${process.env.DATABASE_URL}`;
 const pool = new Pool({ connectionString, max: 1 });
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
-async function main() {}
+import { Level } from "../../generated/prisma/client";
+import { Character } from "../../generated/prisma/client";
+
+async function main(): Promise<void> {
+  //init the games
+  const gameEasy = await prisma.game.create({
+    data: {
+      level: Level.Easy,
+    },
+  });
+  const gameMedium = await prisma.game.create({
+    data: {
+      level: Level.Medium,
+    },
+  });
+  const gameGodlike = await prisma.game.create({
+    data: {
+      level: Level.God,
+    },
+  });
+  const gameHard = await prisma.game.create({
+    data: {
+      level: Level.Hard,
+    },
+  });
+
+  //fill up the chrachter coordinates
+}
 main()
   .then(async () => {
     await prisma.$disconnect();

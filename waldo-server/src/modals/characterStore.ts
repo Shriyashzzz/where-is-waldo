ee35373d1eff1ff634aad8df7a0a478cdd369495
Shriyash <ghimires1@my.smccd.edu>
2026-09-09 16:17:09 -0700

    feat: added temp state module for every game & tighten type checks

--- waldo-server/src/modals/characterStore.ts
+++ waldo-server/src/modals/characterStore.ts
@@ -1,21 +1,49 @@
-type Avatar = {
-  [key: string]: boolean;
-};
+type CharacterName =
+  | "YellowWaldo"
+  | "Dog"
+  | "GirlWaldo"
+  | "Waldo"
+  | "GandalfWaldo";
 
-interface CharachterState {
-  avatars: Avatar;
-  found: (key: string) => void;
+interface CharacterState {
+  avatars: Array<Record<CharacterName, boolean>>;
+  found: (index: number, key: CharacterName) => void;
 }
 
-export const characterState: CharachterState = {
-  avatars: {
-    YellowWaldo: false,
-    Dog: false,
-    GirlWaldo: false,
-    Waldo: false,
-    GandalfWaldo: false,
-  },
-  found: (key: string) => {
-    characterState.avatars[key] = true;
+export const characterState: CharacterState = {
+  avatars: [
+    {
+      YellowWaldo: false,
+      Dog: false,
+      GirlWaldo: false,
+      Waldo: false,
+      GandalfWaldo: false,
+    },
+    {
+      YellowWaldo: false,
+      Dog: false,
+      GirlWaldo: false,
+      Waldo: false,
+      GandalfWaldo: false,
+    },
+    {
+      YellowWaldo: false,
+      Dog: false,
+      GirlWaldo: false,
+      Waldo: false,
+      GandalfWaldo: false,
+    },
+    {
+      YellowWaldo: false,
+      Dog: false,
+      GirlWaldo: false,
+      Waldo: false,
+      GandalfWaldo: false,
+    },
+  ],
+  found: (index: number, key: CharacterName) => {
+    const avatar = characterState.avatars[index];
+    if (!avatar) throw new Error(`Invalid Index value, passed index: ${index}`);
+    avatar[key] = true;
   },
 };

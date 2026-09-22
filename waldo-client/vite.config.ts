56616b3226effed5bd221c12a338ca0344704524
Shriyash <ghimires1@my.smccd.edu>
2026-09-22 00:21:50 -0700

    chore: config

--- waldo-client/vite.config.ts
+++ waldo-client/vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
-
   return {
     plugins: [react(), tailwindcss()],
     envDir: "./",
